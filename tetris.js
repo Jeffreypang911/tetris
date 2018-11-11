dd9f2edc3176a0d355c68791a4fd23d1531152fd
Jeffreypang911 <Jeffreypang911@gmail.com>
2018-11-11 13:31:41 -0800

    adds function to render t piece

--- tetris.js
+++ tetris.js
@@ -3,9 +3,23 @@ const context = canvas.getContext('2d');
 
 context.fillStyle = "#000";
 context.fillRect(0, 0, canvas.width, canvas.height)
+context.scale(20, 20);
 
 const matrix = [
     [0, 0, 0],
     [1, 1, 1],
     [0, 1, 0]
 ]
+
+function drawMatrix(matrix) {
+    matrix.forEach((row, y) => {
+        row.forEach((value, x) => {
+            if (value !== 0) {
+                context.fillStyle = 'red'
+                context.fillRect(x, y, 1, 1);
+            };
+        });
+    });
+};
+
+drawMatrix(matrix);
