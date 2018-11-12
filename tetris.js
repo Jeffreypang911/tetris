56c51bd1c4b3936d59f1b70473a6119bea9a7282
Jeffreypang911 <Jeffreypang911@gmail.com>
2018-11-12 11:06:10 -0800

    updates piece to now fall basted on drop Interval

--- tetris.js
+++ tetris.js
@@ -28,8 +28,20 @@ function drawMatrix(matrix, offset) {
     });
 };
 
+let dropCounter = 0
+let dropInterval = 1000;
+
+let lastTime = 0;
 function update(time = 0) {
-    console.log(time)
+    const deltaTime = time - lastTime;
+    lastTime = time;
+    
+    dropCounter += deltaTime;
+    if (dropCounter > dropInterval){
+        player.pos.y++;
+        dropCounter = 0;
+    }
+
     draw();
     requestAnimationFrame(update);
 }
