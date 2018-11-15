d6c20f788af1e0f75986b82f6251494adbf68527
Jeffreypang911 <Jeffreypang911@gmail.com>
2018-11-14 18:38:59 -0800

    adds floor collision logic once piece reaches bottom

--- tetris.js
+++ tetris.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('tetris');
 const context = canvas.getContext('2d');
 
 
-context.scale(20, 20);
+context.scale(16, 16);
 
 let matrix = [
     [0, 0, 0],
@@ -52,7 +52,7 @@ function update(time = 0) {
     lastTime = time;
     
     dropCounter += deltaTime;
-    if (dropCounter > dropInterval){
+    if (dropCounter > dropInterval && player.pos.y <= 22){
         player.pos.y++;
         dropCounter = 0;
     }
@@ -75,18 +75,18 @@ const player = {
 document.onkeydown = keypress
 function keypress(e) {
     console.log(e.key)
-    if(e.key==="ArrowLeft"){
+    if(e.key==="ArrowLeft" && player.pos.y <= 22){
         player.moveleft
         console.log(player.moveleft())
     }
-    if(e.key==="ArrowRight"){
+    if(e.key==="ArrowRight" && player.pos.y <= 22){
         player.moveright
         console.log(player.moveright())
     }
-    if(e.key==="ArrowUp"){
+    if(e.key==="ArrowUp" && player.pos.y <= 22){
         rotateMatrix(matrix);
     }
-    if(e.key==="ArrowDown"){
+    if(e.key==="ArrowDown" && player.pos.y <= 22){
         player.pos.y++;
     }
 }
