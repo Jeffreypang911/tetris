9aa4f0735adbf2158be11b7a181f10963613ffde
Jeffreypang911 <Jeffreypang911@gmail.com>
2018-11-16 17:35:34 -0800

    fixes formatting

--- tetris.js
+++ tetris.js
@@ -52,7 +52,7 @@ function update(time = 0) {
     lastTime = time;
     
     dropCounter += deltaTime;
-    if (dropCounter > dropInterval && player.pos.y <= 22){
+    if (dropCounter > dropInterval && player.pos.y < 21){
         player.pos.y++;
         dropCounter = 0;
     }
@@ -75,22 +75,22 @@ const player = {
 document.onkeydown = keypress
 function keypress(e) {
     console.log(e.key)
-    if(e.key==="ArrowLeft" && player.pos.y <= 22){
+    if(e.key==="ArrowLeft" && player.pos.y < 22){
         player.moveleft
         console.log(player.moveleft())
     }
-    if(e.key==="ArrowRight" && player.pos.y <= 22){
+    if(e.key==="ArrowRight" && player.pos.y < 22){
         player.moveright
         console.log(player.moveright())
     }
-    if(e.key==="ArrowUp" && player.pos.y <= 22){
+    if(e.key==="ArrowUp" && player.pos.y < 22){
         rotateMatrix(matrix);
     }
-    if(e.key==="ArrowDown" && player.pos.y <= 22){
+    if(e.key==="ArrowDown" && player.pos.y < 22){
         player.pos.y++;
     }
-    if(e.key===" " && player.pos.y <= 22){
-        player.pos.y = 23;
+    if(e.key===" " && player.pos.y < 22){
+        player.pos.y = 22;
     }
 }
 
